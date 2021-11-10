8d2530027068d3d1aab804656f0d2104459d73d9
diegoapb <parra.diego@correounivalle.edu.co>
2021-11-10 12:19:54 -0500

    ADD Bucket,Output

--- lib/demo-cdk-bucket-stack.ts
+++ lib/demo-cdk-bucket-stack.ts
@@ -1,15 +1,28 @@
 import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+// import Bucket
+import { Bucket } from '@aws-cdk/aws-s3';
+// import CfnOutput, CfnParameter, Duration
+import { CfnOutput, CfnParameter, Duration } from '@aws-cdk/core';
+
 
 export class DemoCdkBucketStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create a new bucket as someBucket with lifecycle expiration in 1 day
+    const someBucket = new Bucket(this, 'someBucket', {
+      lifecycleRules: [
+        {
+          expiration: Duration.days(1),
+          enabled: true,
+        },
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'DemoCdkBucketQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create a new CfnOutput with someBucket's bucket name
+    new CfnOutput(this, 'someBucketName', {
+      value: someBucket.bucketName,
+    });
+
   }
 }
