a58d2ba9e1f11695699e690b090e56401365edb9
diegoapb <parra.diego@correounivalle.edu.co>
2021-11-10 12:30:54 -0500

    ADD parameter

--- lib/demo-cdk-bucket-stack.ts
+++ lib/demo-cdk-bucket-stack.ts
@@ -8,13 +8,22 @@ import { CfnOutput, CfnParameter, Duration } from '@aws-cdk/core';
 export class DemoCdkBucketStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    /**
+     * new CfnParameter as duration
+     * @summary is a number, defaul =6 hours
+     */
+    const duration = new CfnParameter(this, 'duration', {
+      type: 'Number',
+      default: 6,
+      minValue: 1,
+      maxValue: 24,
+    });
 
     // Create a new bucket as someBucket with lifecycle expiration in 1 day
     const someBucket = new Bucket(this, 'someBucket', {
       lifecycleRules: [
         {
-          expiration: Duration.days(1),
-          enabled: true,
+          expiration: Duration.days(duration.valueAsNumber),
         },
       ],
     });
